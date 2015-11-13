b0a563f70049710f84135fc0196efdaae0119d85
lmallin <Lucy.Mallin@nbt.nhs.uk>
2015-11-13 04:25:16 -0800

    Delete LRG_import_xml.py

--- LRG_import_xml.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as tree
-
-#parses xml file
-LRG_tree= tree.parse('LRG_263.xml')
-root = LRG_tree.getroot()
-
-#should print out "1.9" if worked
-print root.attrib['schema_version']
